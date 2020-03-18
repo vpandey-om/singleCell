a20a256dc3242f68f8c6e904b4551c8fe18ee669
vikash <vkpepfl@gmail.com>
2020-03-18 16:15:45 +0100

    scp is working

--- cram_to_count_bash.py
+++ cram_to_count_bash.py
@@ -1,5 +1,5 @@
 import glob
-import os 
+import os
 import subprocess
 import sys
 
@@ -12,16 +12,16 @@ from scp import SCPClient
 def main_script():
 	''' This script is written to download cram files form obilab server and do analysis to get count matarix '''
 	print ("start coppy")
-	source=
-	destination=
-	apply_scp("source", "destination")
-	
+	source='/Users/vikash/git-hub/data'
+	destination='/proj/snic2019-8-97/sc_Data'
+	apply_scp(source, destination)
+
 
 
 def apply_scp(source, destination):
 	''' we can use scp to copy from the remote server to the working directory '''
 	## source is the folder with path: we wnat to copy from
-	## destination is the folder with path: where we want to send the files  
+	## destination is the folder with path: where we want to send the files
 	ssh = SSHClient()
 	ssh.load_system_host_keys()
 	#import pdb;pdb.set_trace()
@@ -29,7 +29,7 @@ def apply_scp(source, destination):
 
 	# SCPCLient takes a paramiko transport as an argument
 	scp = SCPClient(ssh.get_transport(),progress4=progress4)
-	
+
 	##
 	# Uploading the 'test' directory with its content in the
 	# '/home/user/dump' remote directory
